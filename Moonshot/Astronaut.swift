284f374b3c5974c58a5df88e5cbb4a45f6eeb67c
Miguel Quezada <mig.aquezada@gmail.com>
2022-06-01 23:03:41 -0400

    Complete app

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,16 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Miguel Quezada on 01-06-22.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    
+    let id: String
+    let name: String
+    let description: String
+
+}
